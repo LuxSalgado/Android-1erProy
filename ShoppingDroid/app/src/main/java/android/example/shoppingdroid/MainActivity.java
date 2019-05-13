f4820b4b911b5f1a0c108079eaaddbfb9cb5e979
daloyo95 <39608985+daloyo95@users.noreply.github.com>
2019-05-13 13:55:18 -0400

    zdvzd
    sa

--- ShoppingDroid/app/src/main/java/android/example/shoppingdroid/MainActivity.java
+++ ShoppingDroid/app/src/main/java/android/example/shoppingdroid/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int las=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
