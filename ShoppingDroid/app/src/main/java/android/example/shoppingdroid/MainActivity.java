53550deeb0eb62d9938032caaf3bc7e0ec93c838
daloyo95 <39608985+daloyo95@users.noreply.github.com>
2019-05-14 22:03:15 -0400

    Empezar recibir datos(me equivoque de branch)

--- ShoppingDroid/app/src/main/java/android/example/shoppingdroid/MainActivity.java
+++ ShoppingDroid/app/src/main/java/android/example/shoppingdroid/MainActivity.java
@@ -3,6 +3,7 @@ package android.example.shoppingdroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,8 +11,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public static final int TEXT_REQUEST = 1;
+    private int numeroProducto = 0;
     TextView Producto1, Producto2, Producto3, Producto4, Producto5,
-             Producto6, Producto7, Producto8,Producto9, Producto10;
+            Producto6, Producto7, Producto8, Producto9, Producto10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,26 +38,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void abrirAgregarProducto(View view) {
-        Intent intent = new Intent (this, AgregarProducto.class);
-        startActivityForResult (intent, TEXT_REQUEST);
+        Intent intent = new Intent(this, AgregarProducto.class);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        String textoProducto = data.getStringExtra(AgregarProducto.EXTRA_AGREGAR);
         if (requestCode == 1) {
 
             if (resultCode == RESULT_OK) {
-                    /**AsignarProductos();*/
-                } else
-                    Toast.makeText(getApplicationContext(), "No se selecciono un producto.", Toast.LENGTH_SHORT).show();
+                AsignarProductos(numeroProducto, textoProducto);
+                numeroProducto++;
+                Toast.makeText(getApplicationContext(), "Producto agregado exitosamente.", Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(getApplicationContext(), "No se selecciono un producto.", Toast.LENGTH_SHORT).show();
 
 
         }
 
     }
 
+    private void AsignarProductos(int i, String texto) {
 
+        switch (i) {
+            case 0:
+                Producto1.setText(texto);
+                break;
+            case 1:
+                Producto2.setText(texto);
+                break;
+            case 2:
+                Producto3.setText(texto);
+                break;
+            case 3:
+                Producto4.setText(texto);
+                break;
+            case 4:
+                Producto5.setText(texto);
+                break;
+            case 5:
+                Producto6.setText(texto);
+                break;
+            case 6:
+                Producto7.setText(texto);
+                break;
+            case 7:
+                Producto8.setText(texto);
+                break;
+            case 8:
+                Producto9.setText(texto);
+                break;
+            case 9:
+                Producto10.setText(texto);
+                break;
+            default:
+                Log.d("ERROR", "Capacidad máxima de productos");
+        }
+
+    }
 
 
 }
